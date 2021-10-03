37137d1dfc55f8d1d4a2bccbb27ad646e9c738b8
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2021-10-03 16:00:48 +0200

    Renommage des dossiers

--- fiche-01_bis/dom-event/index.js
+++ fiche-01_bis/dom-event/index.js
@@ -1,16 +1,20 @@
 "use strict";
 let btn = window.document;
-let message = document.getElementById("message");
+/*let compteur = document.getElementById("count").innerHTML;*/
+
+
+document.getElementById("message").innerHTML = "Êtes vous bon en clickage ?";
 
 function clicker(){
+    /*compteur = parseInt(compteur) + 1;    MARCHE PAS ???*/
     document.getElementById("count").innerHTML = parseInt(document.getElementById("count").innerHTML) + 1;
     if(document.getElementById("count").innerHTML >= 5 && document.getElementById("count").innerHTML < 10){
         document.getElementById("message").innerHTML = " Bravo, bel échauffement !";
     }
     else if(document.getElementById("count").innerHTML >= 10){
         document.getElementById("message").innerHTML = "Vous êtes passé maître en l’art du clic !";
-    }
-    
+    } 
+        
 }
 
 btn.addEventListener("click", clicker);
