8442569c75579b9300cda56a268a0ff346ff6353
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2021-10-05 10:23:11 +0200

    myMoovies : step 2 : dynamic homepage

--- fiche-02/mymoovies/src/index.js
+++ fiche-02/mymoovies/src/index.js
@@ -1,30 +1,36 @@
 // Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // Custom styles
-import './stylesheets/main.css';
-import conjuring3 from './img/conjuring3.jpg';
-import ff9 from './img/fastAndFurious9.jpg';
-
-
+import "./stylesheets/main.css";
+import conjuring3 from "./img/conjuring3.jpg";
+import ff9 from "./img/fastAndFurious9.jpg";
 
 // This is the entry point to your app : add all relevant import and custom code
 
 let page = document.getElementById("page");
 let htmlText = `<h1> Mes films </h1> <ul>`;
 
-const LIBRARIES = [
-    conjuring3,
-    ff9
-];
-const descriptionFIlms = [
-    "Conjuring est un film d'horreur, de troisème édition",
-    "Fast and Furious est un film d'action, de neuvième édition"
-];
 
-for(let i=0; i<LIBRARIES.length; i++){
-    htmlText += `<li><img src='${LIBRARIES[i]}' alt="photo"> ${descriptionFIlms[i]} </li>`
+// test orienté objet
+let conjuring = {
+  name: "conjuring3",
+  description: "Conjuring est un film d'horreur, de troisème édition",
+  getImage: () => `<img src="${conjuring3}" </img>`,
+};
+
+let fastAndFurious = {
+  name: "fast and furious 9",
+  description: "Fast and Furious est un film d'action, de neuvième édition",
+  getImage: () => `<img src="${ff9}"</img>`,
+};
+const LIBRARIES = [conjuring, fastAndFurious];
+
+for (let i = 0; i < LIBRARIES.length; i++) {
+  htmlText += "Film " + (i+1) + " :" + 
+    `<li>${LIBRARIES[i].name} </li>
+    <li>${LIBRARIES[i].getImage()}</li>
+    <li>${LIBRARIES[i].description}</li>
+    <br/>`;
 }
 htmlText += `</ul>`;
 page.innerHTML = htmlText;
-
-
