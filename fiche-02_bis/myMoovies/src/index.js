380e1cb527e2b4201be56bfb35bc37990afe80e1
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2021-11-04 16:25:30 +0100

    myMoovies : step 2 : dynamic homepage

--- fiche-02_bis/myMoovies/src/index.js
+++ fiche-02_bis/myMoovies/src/index.js
@@ -1,8 +1,34 @@
+import conjuringImage from "./img/conjuring3.jpg";
+import fastAndFuriousImage from "./img/fastAndFurious9.jpg";
 // Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Custom styles
-import './stylesheets/main.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 // This is the entry point to your app : add all relevant import and custom code
+
+const homePage = `
+<div class="text-center">
+  <h3>Welcome to myMoovies !</h3>
+
+  <p>Here you can find a selection of our favorite moovies ; )</p>
+  <div class="pb-3">
+    <img 
+        class="img-thumbnail w-50" 
+        src="${conjuringImage}" 
+        alt="Conjuring 3" />
+  </div>
+
+  <div>
+    <img
+      class="img-thumbnail w-50"
+      src="${fastAndFuriousImage}"
+      alt="Fast and Furious 9"
+    />
+  </div>
+</div>`;
+
+const main = document.querySelector("main");
+
+main.innerHTML = homePage;
+
 
