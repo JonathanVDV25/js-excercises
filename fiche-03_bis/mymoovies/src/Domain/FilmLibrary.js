d9962369bedbbc6451adae436944b5df5e8d94a0
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2021-11-18 21:31:42 +0100

    Fixed Fiche03 - mymoovies

--- fiche-03_bis/mymoovies/src/Domain/FilmLibrary.js
+++ fiche-03_bis/mymoovies/src/Domain/FilmLibrary.js
@@ -1,45 +1,49 @@
 class FilmLibrary {
   constructor() {
-    this.FilmLibrary = [];
+    this.filmLibrary = [];
   }
 
   addFilm(film) {
     if (!film) return false;
-    this.FilmLibrary.push({ ...film, id: this.nextFilmId() });
+    // Shallow cloning the film via the Spread operator `...` : {...}
+    // updating the film id : this is the moment to provide an id when the film is added to the collection !
+    this.filmLibrary.push({ ...film, id: this.nextFilmId() });
     return true;
   }
 
   getHtmlTable() {
-    let htmlTable = `<div class="table-responsive p-5>
-        <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Duration (min)</th>
-                <th scope="col">Budget (million)</th>
-            </tr>
-        </thead>
-        <tbody>`;
-    if (this.FilmLibrary && this.FilmLibrary.length > 0) {
-      this.FilmLibrary.forEach((element) => {
+    let htmlTable = `<div class="table-responsive p-5">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Duration (min)</th>
+      <th scope="col">Budget (million)</th>    
+    </tr>
+  </thead>
+  <tbody>`;
+    if (this.filmLibrary && this.filmLibrary.length > 0) {
+      this.filmLibrary.forEach((element) => {
         htmlTable += "<tr>";
+        // Deal with Title Col (create hyperlink from title & link)
         htmlTable += `<td>
-                <a href="${element.link}" target="_blank"> ${element.title} </a> 
-                </td>`; //solution prof : target="_blank"">
-        htmlTable += `<td> ${element.duration} </td>
-                              <td> ${element.budget} </td>
-                              </tr>`;
+        <a href="${element.link}" target="_blank""> ${element.title}</a>      
+      </td>`;
+        // Deal with simple columns (duration & budget)
+        htmlTable += `<td>${element.duration}</td>
+        <td>${element.duration}</td>
+      </tr>`;
       });
     }
     htmlTable += `</tbody>
-    </table>
-    </div>`;
+</table>
+</div>`;
 
     return htmlTable;
   }
 
   nextFilmId() {
-    return this.FilmLibrary.length;
+    return this.filmLibrary.length;
   }
 }
 
